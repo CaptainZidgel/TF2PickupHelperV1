a2d6a8d6624937eebbeec2ce93badb3b069be09a
Michael <thesentientgelatin@gmail.com>
2019-11-27 22:07:47 -0800

    Verbose commenting, removed old roll feature and some old debuggers

--- pugcityhelper.lua
+++ pugcityhelper.lua
@@ -14,14 +14,12 @@ admins = { --there should be an easier way to do this by simply grabbing the lis
 
 usersAlpha = {}		--creates a table of users for use in alphabetizing
 pastmedics = {}
-needtoMed = {}
 
 piepan.On("connect", function()
 	print("Loaded Poopy Joe")
-	root = piepan.Channels[0]
+	root = piepan.Channels[0]			--lua indexes at 1, but GO indexes at 0 so all gumble objects will index at 0.
 	addup = root:Find("Inhouse Pugs (Nut City)", "Add Up")
 	fatkids = root:Find("Inhouse Pugs (Nut City)", "Add Up", "Fat Kids")
-	mowing = root:Find("General", "mowing the lawn")
 	spacebase = root:Find("Inhouse Pugs (Nut City)", "Poopy Joes Space Base")
 	pugroomone = root:Find("Inhouse Pugs (Nut City)", "Add Up", "Pug Server 1")
 	piepan.Self:Move(spacebase)
@@ -35,58 +33,49 @@ function senderIsAdmin(s)
 	end
 end
 
-function checkMedsHist(m) --we pass the Name to this param, not the User object
-	for _,i in ipairs(pastmedics) do
-		print(i)
-		if i == m then
+function checkMedsHist(newmed) --we pass the Name to this param, not the User object
+	for _,oldmedic in ipairs(pastmedics) do
+		print(oldmedic)
+		if oldmedic == newmed then
 			return true
 		end
 	end
 end
 
 function roll()
-	 usersAlpha = {}				--if the users table is empty, generate it
-		for _,i in addup:Users() do			--iterates on all users added up
-			table.insert(usersAlpha, i.Name)--places them into talbe
-		end					
-		table.sort(usersAlpha)
-	
+	usersAlpha = {}				--if the users table is empty, generate it
+	for _,u in addup:Users() do			--iterates on all users added up, "u" is one user
+		table.insert(usersAlpha, u.Name)--places them into talbe
+	end					
+	table.sort(usersAlpha)
 	math.randomseed(os.time())				--seed based on current second
 	local n = math.random(1, #addup.Users)	--pick random player
-	local u = usersAlpha[n]					--find player based on their position in an alphabetized list (the way mumble sorts users in a channel)
-	for _,v in ipairs(pastmedics) do		--iterates on list of previous medics.
-		if v:lower() == u:lower() then
-			if #pastmedics < #addup.Users then
+	local userToTest = usersAlpha[n]					--find player based on their position in an alphabetized list (the way mumble sorts users in a channel)
+	for _,pmedic in ipairs(pastmedics) do		--iterates on list of previous medics. pmedic is a previous medic --this whole loop needs to be rewritten i think, to be clear
+		if pmedic:lower() == userToTest:lower() then
+			if #pastmedics < #addup.Users - 1 then
 				return roll()
 			else
-				print("Something something something")
+				print("Run out of players to test")
 				break
 			end
 		end
 	end
-	print(tostring(n) .. " " .. u)
-	table.insert(pastmedics, u)
+	print(tostring(n) .. " " .. userToTest)
+	table.insert(pastmedics, userToTest)
 	return n                                --returns the random number
 end
 
 piepan.On("userChange", function(u)
-	if u.IsConnected then
+	--[[if u.IsConnected then --cant figure out how to get this to work. It would be nice to automatically add users to usersAlpha on connection, then we would only need to resort on each roll (instead of refinding the entire table), i.e. we could eliminate the for loop that's currently at line 47-49.
 		print("Connection")
-	end
+	end]]--
 end)
 
 piepan.On("message", function(m)
 	if m.sender == nil then
 		return
 	else
-		if m.Message == "!clearmh" then
-			pastmedics = {}
-		end
-		if m.Message == "!dump" then
-			for _,i in ipairs(pastmedics) do
-				print(i)
-			end
-		end
 		if m.Message == "!name" then
 			m.Sender.Channel:Send("Your name is " .. m.Sender.Name .. "!", false)
 		end
@@ -94,79 +83,44 @@ piepan.On("message", function(m)
 			if string.find(m.Message, "!echo ", 1) == 1 then
 				piepan.Self.Channel:Send(m.Message:sub(7), false)
 			end
-			if string.find(m.Message, "!pick ", 1) == 1 then
-				
-			end
-			if m.Message == "!iter" then
-				for _,i in fatkids:Users() do
-					print(i.Name)
-					i:Move(addup)
-				end
-			end
 			if m.Message == "!cdump 1" then
 				m.Sender.Channel:Send("Attempting to dump channels...", true)
 				local red = pugroomone:Find("Red")
 				local blu = pugroomone:Find("Blu")
-				for _,i in red:Users() do
-					i:Move(addup)
+				for _,u in red:Users() do -- "u" is one user
+					u:Move(addup)
 				end
-				for _,i in blu:Users() do
-					i:Move(addup)
+				for _,u in blu:Users() do
+					u:Move(addup)
 				end
 				addup:Send("Dumped by " .. m.Sender.Name, true)
 			end
 			if m.Message == "!roll" then
-				local n1 = roll()
-				local n2 = roll()
-				redMed = usersAlpha[n1]
-				bluMed = usersAlpha[n2]
-				addup:Send("Medics are " .. redMed .. " (" .. n1 .. ") and " .. bluMed .. " (" .. n2 .. ")", true) --sends the number and name of the Medics to addup channel.
-				addup:Send("Rolled by " .. m.Sender.Name, true)
-			end
-			if m.Message == "archived!roll" then
 				if #addup.Users + #fatkids.Users < 1 then --checks if there are enough players to play (While Mumble counts users in a channel with children included, piepan strictly only counts the number of users in a specific channel. This is helpful).
 					addup:Send("Sorry, there are not enough players to do this pug.", true)
 				else
-					--for _,i in ipairs(usersAlpha) do print(i) end
-					math.randomseed(os.time())			--seeds a new random number based on the current second
-					local _random = tostring(math.random(1, #addup.Users)) --picks a random int from 1 to the number of users in addup channel
-					local _random2 = tostring(math.random(1, #addup.Users))
-					while _random2 == _random do							--if number 1 and number 2 are the same, reroll until we get a unique number
-						_random2 = tostring(math.random(1, #addup.Users))
-					end
-					redMed = usersAlpha[tonumber(_random)]
-					bluMed = usersAlpha[tonumber(_random2)]
-					while checkMedsHist(redMed) do					--when this line is executed, if the condition is met the loop will begin and continue until it is not met, if the condition is not met, the loop will not happen (If I understand while loops correctly)
-						local n = _random
-						addup:Send("Rolled a dupe, rerolling", true)
-						while (_random == n) or (_random == _random2) do
-							_random = tostring(math.random(1, #addup.Users))
-						end
-					end
-					while checkMedsHist(blueMed) do
-						local n = _random2
-						addup:Send("Rolled a dupe, rerolling", true)
-						while (_random2 == n) or (_random2 == _random) do
-							_random2 = tostring(math.random(1, #addup.Users))
-						end
-					end
-					table.insert(pastmedics, redMed)
-					table.insert(pastmedics, bluMed)
-					addup:Send(_random .. " " .. _random2, true)			--sends the numbers in plaintext (debugging)
-					addup:Send("Medics are " .. redMed .. " (" .. _random .. ") and " .. bluMed .. " (" .. _random2 .. ")", true) --sends the number and name of the Medics to addup channel.
-					addup:Send("Rolled by " .. m.Sender.Name, true)
-					
-					
+					local n1 = roll()		--if i wrote the roll function correctly, it should keep rerolling until there are two unique medics, meaning it shouldnt proceed until it finds a valid medic.
+					local n2 = roll()
+					redMed = usersAlpha[n1] --redmedic is the nth user (the n1 value)
+					bluMed = usersAlpha[n2] --blumedic is the nth user, but with the other n value (n2)
+					addup:Send("Medics are " .. redMed .. " (" .. n1 .. ") and " .. bluMed .. " (" .. n2 .. ")", false) --sends the number and name of the Medics to addup channel.
+					addup:Send("Rolled by " .. m.Sender.Name, false) --transparency logging
 				end
 			end
-			if m.Message == "!s" then
+			if m.Message == "!s" then			--prints channels and their ids
 				for i,c in piepan.Channels() do
 					print(c.Name .. " " .. c.ID)
 				end
 			end
-			if m.Message == "!move" then
-				m.Sender:Move(addup)
+			if m.Message == "!clearmh" then	--clears medic history
+				pastmedics = {}
 			end
+			if m.Message == "!pmh" then		--prints every past medic (for debugging purposes)
+				for _,i in ipairs(pastmedics) do
+					print(i)
+				end
+			end
+			
 		end
 	end
 end)
